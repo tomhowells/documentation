77b8923a53803f1c8c34211ada1807bff2aaf563
Ned Carreon <ned.carreon@argyledata.com>
2015-11-20 11:49:43 -0800

    Fix RedisStore for RedisToGO

--- site/index.js
+++ site/index.js
@@ -6,9 +6,8 @@ var express = require('express'),
     session = require('express-session'),
     url = require('url'),
     RedisStore = require('connect-redis')(session),
-    //cookieParser = require('cookie-parser'),
     path = require('path'),
-    domain = require('domain');
+    domain = require('domain'),
     authorization = require('./usermanage/Authorization');
 
 
@@ -20,59 +19,25 @@ _createWorkerApp();
 function _createWorkerApp(){
     var app = express();
     var port = process.env.PORT || 8080;
-    // session support
-    //app.use(cookieParser('720657C2-5890-4F3F-838C-F056C64AE304'));
-/*
-    app.use(session({
-        secret: '720657C2-5890-4F3F-838C-F056C64AE304',
-        resave: false,
-        saveUninitialized: false,
-        store: new RedisStore({
-            host: "localhost",
-            port: 8083,
-            ttl:  1800  //  expiring session after xx minutes
-        })
-    }));
-*/
-    // RedisToGo connection
-/*
+    // Session support using RedisToGo
     app.configure(function () {
-        var redisUrl = url.parse(process.env.REDISTOGO_URL),
-            redisAuth = redisUrl.auth.split(':');
-        console.log("redisUrl:", JSON.stringify(redisUrl));
-        console.log("redisAuth:", redisAuth.join(','));
+         var redisUrl = url.parse("redis://redistogo:4070e1b91adbd692981a386b1d66af4a@panga.redistogo.com:9898/"),
+         redisAuth = redisUrl.auth.split(':');
+         console.log("redisUrl:", JSON.stringify(redisUrl));
+         console.log("redisAuth:", redisAuth.join(','));
 
-        //redisUrl = {"protocol":"redis:","slashes":true,"auth":"redistogo:4070e1b91adbd692981a386b1d66af4a","host":"panga.redistogo.com:9898","port":"9898","hostname":"panga.redistogo.com","hash":null,"search":null,"query":null,"pathname":"/","path":"/","href":"redis://redistogo:4070e1b91adbd692981a386b1d66af4a@panga.redistogo.com:9898/"};
-        app.use(session({
-            secret: '720657C2-5890-4F3F-838C-F056C64AE304',
-            store: new RedisStore({
-                host: redisUrl.hostname,
-                port: redisUrl.port,
-                db: 1, //redisAuth[0],
-                pass: redisAuth[1]
-            })
-        }));
+         //redisUrl = {"protocol":"redis:","slashes":true,"auth":"redistogo:4070e1b91adbd692981a386b1d66af4a","host":"panga.redistogo.com:9898","port":"9898","hostname":"panga.redistogo.com","hash":null,"search":null,"query":null,"pathname":"/","path":"/","href":"redis://redistogo:4070e1b91adbd692981a386b1d66af4a@panga.redistogo.com:9898/"};
+         app.use(session({
+         secret: '720657C2-5890-4F3F-838C-F056C64AE304',
+         store: new RedisStore({
+         host: redisUrl.hostname,
+         port: redisUrl.port,
+         //db: redisAuth[0],
+         pass: redisAuth[1]
+         })
+         }));
     });
-*/
-
-    var redisUrl = url.parse(process.env.REDISTOGO_URL),
-        redisAuth = redisUrl.auth.split(':');
-    console.log("redisUrl:", JSON.stringify(redisUrl));
-    console.log("redisAuth:", redisAuth.join(','));
-
-    //redisUrl = {"protocol":"redis:","slashes":true,"auth":"redistogo:4070e1b91adbd692981a386b1d66af4a","host":"panga.redistogo.com:9898","port":"9898","hostname":"panga.redistogo.com","hash":null,"search":null,"query":null,"pathname":"/","path":"/","href":"redis://redistogo:4070e1b91adbd692981a386b1d66af4a@panga.redistogo.com:9898/"};
-    app.use(session({
-        secret: '720657C2-5890-4F3F-838C-F056C64AE304',
-        store: new RedisStore({
-            host: redisUrl.hostname,
-            port: redisUrl.port,
-            db: 1, //redisAuth[0],
-            pass: redisAuth[1]
-        })
-    }));
-
     app.use(authorization);
-    //app.use(sessionHolder.startup());
     app.use(express.static(clientRoot));
     app.use(express.json());
     app.use(express.urlencoded());
