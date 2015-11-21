04afb5dba6c7d0af38c1e5c83388c53eb2c0fc91
Ned Carreon <ned.carreon@argyledata.com>
2015-11-20 17:11:48 -0800

    Pass 401 on bad username or password

--- site/usermanage/UserManageCtrl.js
+++ site/usermanage/UserManageCtrl.js
@@ -11,35 +11,33 @@ module.exports = function(app) {
     app.post('/signin', function(req, res, next) {
         var userName = req.body.userName;
         var passwd = req.body.passwd;
-        var rtg   = url.parse(process.env.REDISTOGO_URL);
-        var redisClient = redis.createClient(
-            rtg.port,
-            rtg.hostname
-        );
 
-        redisClient.auth(rtg.auth.split(":")[1]);
+        var redisToGoDefaultUrl = "redis://redistogo:4070e1b91adbd692981a386b1d66af4a@panga.redistogo.com:9898/";
+        var redisToGoUrl = process.env.REDISTOGO_URL || redisToGoDefaultUrl;
+        var redisUrlParsed = url.parse(redisToGoUrl);
+        var redisClient = redis.createClient(
+            redisUrlParsed.port,
+            redisUrlParsed.hostname
+        );
+        redisClient.auth(redisUrlParsed.auth.split(":")[1]);
 
         redisClient.on('connect', function(err){
             if (err) return;
-            redisClient.get(userName, function(err, value){
+            redisClient.get(userName, function(err, result){
                 if (err) {
                     return next(err);
                 }
                 else {
-                    if (!value) {
-                        res.json({
-                            message: 'username-failed'
-                        });
+                    if (!result) {
+                        res.status(401).send({ message: "bad username" });
                     }
-                    else if (value === passwd) {
+                    else if (result !== passwd) {
+                        res.status(401).send({ message: "bad password" });
+                    }
+                    else {
                         req.session.userData = userName;
                         res.json({
                             signinStatus: 'verified'
-                        });
-                    }
-                    else {
-                        res.json({
-                            message: 'password-failed'
                         });
                     }
                 }
