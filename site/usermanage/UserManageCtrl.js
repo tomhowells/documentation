cae398831ee1d5e4b54f9ac901696bf98a08cc9b
Ned Carreon <ned.carreon@argyledata.com>
2015-11-20 16:31:32 -0800

    Use password checking

--- site/usermanage/UserManageCtrl.js
+++ site/usermanage/UserManageCtrl.js
@@ -4,16 +4,15 @@
 'use strict';
 
 var redis = require('redis');
+var url = require('url');
 
 module.exports = function(app) {
 
     app.post('/signin', function(req, res, next) {
-        var userName = req.body.userName,
-            passwd = req.body.passwd;
-
-        var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-        //var rtg = require("url").parse("redis://redistogo:4070e1b91adbd692981a386b1d66af4a@panga.redistogo.com:9898/");
-        var redisClient = require("redis").createClient(
+        var userName = req.body.userName;
+        var passwd = req.body.passwd;
+        var rtg   = url.parse(process.env.REDISTOGO_URL);
+        var redisClient = redis.createClient(
             rtg.port,
             rtg.hostname
         );
@@ -22,16 +21,16 @@ module.exports = function(app) {
 
         redisClient.on('connect', function(err){
             if (err) return;
-            console.log("connected as:", userName);
             redisClient.get(userName, function(err, value){
                 if (err) {
                     return next(err);
                 }
-                console.log("username got client: ",value);
-                req.session.userData = userName;
-                res.json({
-                    signinStatus: 'verified'
-                });
+                if (value && value === passwd) {
+                    req.session.userData = userName;
+                    res.json({
+                        signinStatus: 'verified'
+                    });
+                }
 
             });
         });
